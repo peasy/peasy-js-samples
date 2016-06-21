f8dc58f6fd83e994798b9e30a2892025c0ce5e33
ahanusa <aaron.hanusa@gmail.com>
2016-06-20 19:48:04 -0500

    wire up body-parser middleware

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var express = require('express');
-
 var app = express();
+var bodyParser = require('body-parser');
+
+// MIDDLEWARE
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+var customers = require('./server/controllers/customers.js')(app);
 
 app.get('/', function(req, res) {
-  res.send('hello world');
+  //res.send('hello world');
+  res.json({'hi':'world'});
 });
 
 var server = app.listen(3000, function() {
