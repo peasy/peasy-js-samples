fb98a52f6b0726fb4f56ceea82a513dd9d90c3bf
ahanusa <aaron.hanusa@gmail.com>
2016-06-20 19:47:31 -0500

    add customers controller boilerplate

---
+++ server/controllers/customers.js
@@ -0,0 +1,37 @@
+var _ = require('lodash');
+
+module.exports = function(app) {
+
+  var customers = [{id: 1, name: 'aaron hanusa'}];
+
+  // CREATE
+  app.post('/customers', function(req, res) {
+    customers.push(req.body);
+    req.body.foo = "new";
+    res.json(req.body);
+  });
+
+  // RETRIEVE
+  app.get('/customers', (req, res) => {
+    res.send(customers);
+  });
+
+  app.get('/customers/:id', (req, res) => {
+    res.send(customers.map(c => c.id === req.params.id)[0]);
+  });
+
+  // UPDATE
+  app.put('/customers/:id', (req, res) => {
+    var index = customers.findIndex(c => c.id === req.params.id);
+    var customer = customers[index];
+    var result = _.merge(customer, req.body);
+    res.json(result);
+  });
+
+  // DELETE
+  app.delete('/customers/:id', (req, res) => {
+    var index = customers.findIndex(c => c.id === req.params.id);
+    customers.splice(index, 1);
+    res.json("ok");
+  });
+};
