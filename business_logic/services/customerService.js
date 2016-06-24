a4e501de501d394cbbb869d7d3d3db9255867625
ahanusa <aaron.hanusa@gmail.com>
2016-06-24 09:17:25 -0500

    add whitelist functionality to insert/update init methods

--- business_logic/services/customerService.js
+++ business_logic/services/customerService.js
@@ -1,5 +1,25 @@
 var BusinessService = require('peasy-js').BusinessService;
 
-var CustomerService = BusinessService.extend().service;
+function stripValues(customer) {
+  var allowableFields = ['name', 'street'];
+  Object.keys(customer).forEach(function(field) {
+    if (allowableFields.indexOf(field) === -1) {
+      delete customer[field];
+    }
+  });
+}
+
+var CustomerService = BusinessService.extend({
+  functions: {
+    _onInsertCommandInitialization: function(customer, context, done) {
+      stripValues(customer);
+      done();
+    },
+    _onUpdateCommandInitialization: function(customer, context, done) {
+      stripValues(customer);
+      done();
+    }
+  }
+}).service;
 
 module.exports = CustomerService;
