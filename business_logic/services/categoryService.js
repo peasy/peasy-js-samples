e0c8e4cc42081d3b40d2d8cab65b640b9abaddb3
ahanusa <aaron.hanusa@gmail.com>
2016-06-24 09:23:37 -0500

    add categoryService

---
+++ business_logic/services/categoryService.js
@@ -0,0 +1,25 @@
+var BusinessService = require('peasy-js').BusinessService;
+
+function stripValues(customer) {
+  var allowableFields = ['name'];
+  Object.keys(category).forEach(function(field) {
+    if (allowableFields.indexOf(field) === -1) {
+      delete category[field];
+    }
+  });
+}
+
+var CategoryService = BusinessService.extend({
+  functions: {
+    _onInsertCommandInitialization: function(category, context, done) {
+      stripValues(category);
+      done();
+    },
+    _onUpdateCommandInitialization: function(category, context, done) {
+      stripValues(category);
+      done();
+    }
+  }
+}).service;
+
+module.exports = CategoryService;
