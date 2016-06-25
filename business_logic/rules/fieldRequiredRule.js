38e2f790481c8c968bf476aee58e5e194fd700c5
ahanusa <aaron.hanusa@gmail.com>
2016-06-25 13:12:40 -0500

    fix casing bug

--- business_logic/rules/fieldRequiredRule.js
+++ business_logic/rules/fieldRequiredRule.js
@@ -4,6 +4,8 @@ var FieldRequiredRule = Rule.extend({
   params: ['field', 'data'],
   functions: {
     _onValidate: function(done) {
+      console.log("DATA", this.data);
+      console.log("FIELD", this.field);
       if (!this.data[this.field]) {
         this.association = this.field;
         this._invalidate(this.field + " is required");
