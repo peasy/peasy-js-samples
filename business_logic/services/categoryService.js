708926c38c507ffcd09c1210804b696762ad3e0c
ahanusa <aaron.hanusa@gmail.com>
2016-06-25 11:40:47 -0500

    allow parentid field to be manipulated

--- business_logic/services/categoryService.js
+++ business_logic/services/categoryService.js
@@ -5,14 +5,14 @@ var utils = require('../shared/utils');
 var CategoryService = BusinessService.extend({
   functions: {
     _onInsertCommandInitialization: function(category, context, done) {
-      utils.stripAllFieldsFrom(category).except('name');
+      utils.stripAllFieldsFrom(category).except(['name', 'parentid']);
       done();
     },
     _getRulesForInsert: function(category, context, done) {
       done(null, new FieldRequiredRule("name", category));
     },
     _onUpdateCommandInitialization: function(category, context, done) {
-      utils.stripAllFieldsFrom(category).except(['id', 'name']);
+      utils.stripAllFieldsFrom(category).except(['id', 'name', 'parentid']);
       done();
     },
     _getRulesForUpdate: function(category, context, done) {
