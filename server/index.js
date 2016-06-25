56abd05d846ffea2490d834c75c2222c10e793a7
ahanusa <aaron.hanusa@gmail.com>
2016-06-25 13:14:38 -0500

    add products controller

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ var CustomerDataProxy = require('../data_proxies/mongo/customerDataProxy');
 var CustomerService = require('../business_logic/services/customerService');
 var CategoryDataProxy = require('../data_proxies/mongo/categoryDataProxy');
 var CategoryService = require('../business_logic/services/categoryService');
+var ProductDataProxy = require('../data_proxies/mongo/productDataProxy');
+var ProductService = require('../business_logic/services/productService');
 var createController = require('./controllers/createController');
 
 // MIDDLEWARE
@@ -22,6 +24,7 @@ app.use(bodyParser.urlencoded({
 
 createController('/customers', app, new CustomerService(new CustomerDataProxy()));
 createController('/categories', app, new CategoryService(new CategoryDataProxy()));
+createController('/products', app, new ProductService(new ProductDataProxy()));
 
 app.get('/customers/:id/addresses', function(req, res) {
   res.json({'hi':req.params.id});
