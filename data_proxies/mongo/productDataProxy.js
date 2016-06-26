9152be3e54880a9561f826618807e68150b1aba1
ahanusa <aaron.hanusa@gmail.com>
2016-06-26 14:14:14 -0500

    add getByCategory function

--- data_proxies/mongo/productDataProxy.js
+++ data_proxies/mongo/productDataProxy.js
@@ -1,3 +1,4 @@
+var objectId = require('mongodb').ObjectID;
 var MongoDataProxy = require('./mongoDataProxy');
 
 var ProductDataProxy = function() {
@@ -5,5 +6,21 @@ var ProductDataProxy = function() {
 };
 
 ProductDataProxy.prototype = new MongoDataProxy();
+
+ProductDataProxy.prototype.getByCategory = function(categoryId, done) {
+  var self = this;
+  self._mongodb.connect(self.connectionString, function(err, db) {
+    if (err) { done(err); }
+    var collection = db.collection(self.collectionName);
+    collection.findOne({categoryId: categoryId}, function(err, data) {
+      if (data) {
+        data.id = data._id;
+        delete data._id;
+      }
+      db.close();
+      done(err, data);
+    });
+  });
+};
 
 module.exports = ProductDataProxy;
