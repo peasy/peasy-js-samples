4af7d826946d18c0954391393e7847016048135e
ahanusa <aaron.hanusa@gmail.com>
2016-06-26 14:15:22 -0500

    add route to handle products?categoryid

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var utils = require('./utils');
 var InMemoryDataProxy = require('../data_proxies/in-memory/InMemoryDataProxy.js');
 var CustomerDataProxy = require('../data_proxies/mongo/customerDataProxy');
 var CustomerService = require('../business_logic/services/customerService');
@@ -22,17 +23,34 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-createController('/customers', app, new CustomerService(new CustomerDataProxy()));
-createController('/categories', app, new CategoryService(new CategoryDataProxy()));
-createController('/products', app, new ProductService(new ProductDataProxy()));
+var OK = 200;
+var BAD_REQUEST = 400;
+var NOT_FOUND = 404;
 
-app.get('/customers/:id/addresses', function(req, res) {
-  res.json({'hi':req.params.id});
+utils.createController('/customers', app, new CustomerService(new CustomerDataProxy()));
+utils.createController('/categories', app, new CategoryService(new CategoryDataProxy()));
+utils.addRouteHandler(() => {
+  app.get('/products', (req, res) => {
+    var service = new ProductService(new ProductDataProxy());
+    var command = service.getAllCommand();
+    if (req.query.categoryid) {
+      command = service.getByCategoryCommand(req.query.categoryid);
+    }
+    command.execute((err, result) => {
+      if (result.success) {
+        if (result.value) {
+          res.status(OK).json(result.value);
+        } else {
+          res.status(NOT_FOUND).send("");
+        }
+      } else {
+        res.status(BAD_REQUEST).json(result.errors);
+      }
+    });
+  });
 });
+utils.createController('/products', app, new ProductService(new ProductDataProxy()));
 
-app.get('/inventoryItems?:productId', function(req, res) {
-  res.json({'items':req.query.productid});
-});
 
 //app.get('/', function(req, res) {
   ////res.send('hello world');
