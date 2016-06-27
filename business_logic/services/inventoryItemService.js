92c78c194270f48cc23b0b743c1f2920215a80de
ahanusa <aaron.hanusa@gmail.com>
2016-06-27 08:29:19 -0500

    add inventoryItemService

---
+++ business_logic/services/inventoryItemService.js
@@ -0,0 +1,45 @@
+var BusinessService = require('peasy-js').BusinessService;
+var FieldRequiredRule = require('../rules/fieldRequiredRule');
+var FieldLengthRule = require('../rules/fieldLengthRule');
+var FieldTypeRule = require('../rules/fieldTypeRule');
+var utils = require('../shared/utils');
+
+var InventoryItemService = BusinessService.extend({
+  functions: {
+    _onInsertCommandInitialization: function(item, context, done) {
+      utils.stripAllFieldsFrom(item).except(['quantityOnHand', 'productId']);
+      done();
+    },
+    _getRulesForInsert: function(item, context, done) {
+      done(null, [
+        new FieldRequiredRule("quantityOnHand", item)
+             .ifValidThenValidate(new FieldTypeRule("quantityOnHand", item.quantityOnHand, "number")),
+        new FieldRequiredRule("productId", item)
+      ]);
+    },
+    _onUpdateCommandInitialization: function(item, context, done) {
+      utils.stripAllFieldsFrom(item).except(['id', 'quantityOnHand', 'productId', 'version');
+      done();
+    },
+    _getRulesForUpdate: function(item, context, done) {
+      done([
+        new FieldRequiredRule("quantityOnHand", item)
+             .ifValidThenValidate(new FieldTypeRule("quantityOnHand", item.quantityOnHand, "number")),
+        new FieldRequiredRule("productId", item)
+      ]);
+    }
+  }
+}).createCommand({
+  name: 'getByProductCommand',
+  params: ['productId'],
+  functions: {
+    _onValidationSuccess: function(context, done) {
+      this.dataProxy.getByProduct(this.productId, function(err, result) {
+        done(null, result);
+      });
+    }
+  }
+}).service;
+
+
+module.exports = InventoryItemService;
