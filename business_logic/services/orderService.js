25df18a859ef711dea8a9f6e81dba0a3992c8881
ahanusa <aaron.hanusa@gmail.com>
2016-06-28 12:52:48 -0500

    add orderService

---
+++ business_logic/services/orderService.js
@@ -0,0 +1,44 @@
+var BusinessService = require('peasy-js').BusinessService;
+var FieldRequiredRule = require('../rules/fieldRequiredRule');
+var FieldLengthRule = require('../rules/fieldLengthRule');
+var FieldTypeRule = require('../rules/fieldTypeRule');
+var utils = require('../shared/utils');
+
+var OrderService = BusinessService.extend({
+  functions: {
+    _onInsertCommandInitialization: function(customer, context, done) {
+      utils.stripAllFieldsFrom(customer).except('customerId');
+      customer.orderDate = new Date();
+      done();
+    },
+    _getRulesForInsert: function(customer, context, done) {
+      done(null, new FieldRequiredRule("customerId", customer));
+    },
+    _onUpdateCommandInitialization: function(customer, context, done) {
+      utils.stripAllFieldsFrom(customer).except('customerId');
+      done();
+    }
+  }
+}).createCommand({
+  name: 'getByCustomerCommand',
+  params: ['customerId'],
+  functions: {
+    _onValidationSuccess: function(context, done) {
+      this.dataProxy.getByCustomer(this.customerId, function(err, result) {
+        done(null, result);
+      });
+    }
+  }
+}).createCommand({
+  name: 'getByProductCommand',
+  params: ['productId'],
+  functions: {
+    _onValidationSuccess: function(context, done) {
+      this.dataProxy.getByProduct(this.productId, function(err, result) {
+        done(null, result);
+      });
+    }
+  }
+}).service;
+
+module.exports = OrderService;
