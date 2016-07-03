36faede05d6a69983f6d06d6e0c95e244a828225
ahanusa <aaron.hanusa@gmail.com>
2016-07-03 14:48:45 -0500

    introduce baseService

---
+++ business_logic/services/baseService.js
@@ -0,0 +1,32 @@
+var BusinessService = require('peasy-js').BusinessService;
+var FieldRequiredRule = require('../rules/fieldRequiredRule');
+var utils = require('../shared/utils');
+var _ = require('lodash');
+var NotFoundError = require('../shared/notFoundError');
+
+var BaseService = BusinessService.extend({
+  functions: {
+    _update: function(context, done) {
+      var dataProxy = this.dataProxy;
+      var data = this.data;
+      dataProxy.getById(data.id, function(err, result) {
+        if (err) { return done(err); }
+        if (!result) { return done(new NotFoundError(`item ${data.id} not found`)); }
+        var entity = _.merge(result, data);
+        console.log("MERGE");
+        console.log("RESULT", result);
+        console.log("DATA", data);
+        console.log("ENTITY", entity);
+        if (data.version) {
+          entity.version = data.version;
+        }
+        dataProxy.update(entity, function(err, result) {
+          if (err) { return done(err); }
+          done(null, result);
+        });
+      });
+    }
+  }
+}).service;
+
+module.exports = BaseService;
