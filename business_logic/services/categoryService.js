150f53e717e5a13cebf5bdae99903809adf5f5f1
ahanusa <aaron.hanusa@gmail.com>
2016-07-03 16:15:28 -0500

    add getRulesForDestroy implementation

--- business_logic/services/categoryService.js
+++ business_logic/services/categoryService.js
@@ -2,8 +2,10 @@ var BusinessService = require('peasy-js').BusinessService;
 var FieldRequiredRule = require('../rules/fieldRequiredRule');
 var utils = require('../shared/utils');
 var BaseService = require('../services/baseService');
+var CanDeleteCategoryRule = require('../rules/canDeleteCategoryRule');
 
 var CategoryService = BusinessService.extendService(BaseService, {
+  params: ['dataProxy', 'productService'],
   functions: {
     _onInsertCommandInitialization: function(context, done) {
       var category = this.data;
@@ -22,6 +24,10 @@ var CategoryService = BusinessService.extendService(BaseService, {
     _getRulesForUpdateCommand: function(context, done) {
       var category = this.data;
       done(null, new FieldRequiredRule("name", category));
+    },
+    _getRulesForDestroyCommand: function(context, done) {
+      var categoryId = this.id;
+      done(null, new CanDeleteCategoryRule(categoryId, this.productService));
     }
   }
 }).service;
