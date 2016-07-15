02cdce6a9b12bc5a86f9d57f3ab6124c50c391a1
ahanusa <aaron.hanusa@gmail.com>
2016-07-15 13:20:25 -0500

    add concurrency support

--- data_proxies/in-memory/inventoryItemDataProxy.js
+++ data_proxies/in-memory/inventoryItemDataProxy.js
@@ -1,4 +1,5 @@
 var InMemoryDataProxy = require('./inMemoryDataProxy');
+var ConcurrencyError = require('./../../business_logic/shared/concurrencyError');
 var _ = require('lodash');
 
 var InventoryItemDataProxy = function(defaults) {
@@ -9,7 +10,10 @@ InventoryItemDataProxy.prototype = new InMemoryDataProxy();
 
 InventoryItemDataProxy.prototype.update = function(data, done) {
   var existing = this._findBy(data.id);
-  existing.version++;
+  if (existing.version !== data.version) {
+    return done(new ConcurrencyError("This item has been modified, please try again with new version"));
+  }
+  data.version++;
   _.merge(existing, data);
   done(null, Object.assign({}, existing));
 };
