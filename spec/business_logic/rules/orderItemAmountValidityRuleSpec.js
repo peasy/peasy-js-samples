6d9d1b9729c5903b93cbf938374dc2bfd0cf7af7
ahanusa <aaron.hanusa@gmail.com>
2016-08-11 20:42:01 -0500

    add orderItemAmountValidityRuleSpec

---
+++ spec/business_logic/rules/orderItemAmountValidityRuleSpec.js
@@ -0,0 +1,23 @@
+describe("OrderItemAmountValidityRule", function() {
+  var OrderItemAmountValidityRule = require('../../../business_logic/rules/orderItemAmountValidityRule');
+
+  it("invalidates when order item's amount does not equal product price * order items's quanity", () => {
+    var orderItem = { amount: 102, quantity: 5 };
+    var product = { price: 20.2 };
+    var rule = new OrderItemAmountValidityRule(orderItem, product);
+    rule.validate(() => {
+      expect(rule.valid).toBe(false);
+    });
+  });
+
+  it("validates when order item's amount equals product price * order items's quanity", () => {
+    var orderItem = { amount: 101, quantity: 5 };
+    var product = { price: 20.2 };
+    var rule = new OrderItemAmountValidityRule(orderItem, product);
+    rule.validate(() => {
+      expect(rule.valid).toBe(true);
+    });
+  });
+
+
+});
