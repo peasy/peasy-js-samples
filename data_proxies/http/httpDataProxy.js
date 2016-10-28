b8407100129e61d32648248b50246d601bddbddd
ahanusa <aaron.hanusa@gmail.com>
2016-10-28 12:26:54 -0500

    export data proxies

--- data_proxies/http/httpDataProxy.js
+++ data_proxies/http/httpDataProxy.js
@@ -1,63 +1,62 @@
 var axious = require('axios');
+var ServiceException = require('../../node_modules/peasy-js').ServiceException;
+var ConcurrencyError = require('../../business_logic/shared/concurrencyError');
+var NotFoundError = require('../../business_logic/shared/notFoundError');
 
 var HttpDataProxy = function(entity) {
   this._url = `/${entity}`;
 };
 
 HttpDataProxy.prototype.getAll = function(done) {
-  axios.get(this._url)
-       .then(response => done(null, response.data))
-       .catch(done);
-}
+  handleResponseFrom(axios.get(this._url), done)
+};
 
 HttpDataProxy.prototype.getById = function(id, done) {
-  axios.get(`${this._url}/${id}`)
-       .then(response => done(null, response.data))
-       .catch(done);
+  handleResponseFrom(axios.get(`${this._url}/${id}`), done);
 };
 
 HttpDataProxy.prototype.insert = function(data, done) {
-  axios.post(this._url, data)
-       .then(response => done(null, response.data))
-       .catch((err) => {
-         if (err.response.status === 400) {
-           console.log("400 occurred");
-         }
-         done(err);
-       });
+  handleResponseFrom(axios.post(this._url, data), done);
 };
 
 HttpDataProxy.prototype.update = function(data, done) {
-  axios.put(`${this._url}/${id}`, data)
-       .then(response => done(null, response.data))
-       .catch((err) => {
-         if (err.response.status === 400) {
-           console.log("400 occurred");
-         }
-         done(err);
-       });
-};
-
-HttpDataProxy.prototype.update = function(data, done) {
-  axios.put(`${this._url}/${data.id}`, data)
-       .then(response => done(null, response.data))
-       .catch((err) => {
-         if (err.response.status === 400) {
-           console.log("400 occurred");
-         }
-         done(err);
-       });
+  handleResponseFrom(axios.put(`${this._url}/${data.id}`, data), done);
 };
 
 HttpDataProxy.prototype.destroy = function(id, done) {
-  axios.delete(`${this._url}/${id}`)
-       .then(response => done(null, response.data))
-       .catch((err) => {
-         if (err.response.status === 400) {
-           console.log("400 occurred");
-         }
-         done(err);
-       });
+  handleResponseFrom(axios.delete(`${this._url}/${id}`), done);
 };
+
+function handleResponseFrom(promise, done) {
+  promise.then(response => done(null, response.data))
+         .catch((err) => done(getError(err)));
+}
+
+var httpStatusCodes = {
+  badRequest: 400,
+  conflict: 409,
+  notFound: 404,
+  notImplemented: 501
+}
+
+function getError(err) {
+  switch (err.response.status) {
+    case httpStatusCodes.badRequest:
+      var serviceException = new ServiceException(err.message);
+      if (Array.isArray(err.response.data)) {
+        serviceException.errors = err.response.data;
+      }
+      return serviceException;
+
+    case httpStatusCodes.conflict:
+      return new ConcurrencyError(err.message);
+
+    case httpStatusCodes.notFound:
+      return new NotFoundError(err.message);
+
+    default:
+      return err;
+  }
+}
 
 module.exports = HttpDataProxy;
--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,47 @@
 ( function( ordersDotCom ) {
 
   var proxy = new ordersDotCom.dataProxies.CustomerDataProxy();
+  var service = new ordersDotCom.services.CustomerService(proxy);
 
-  proxy.getById(1, (err, result) => {
+  proxy.insert({}, (err, done) => {
+    console.log("ERROR", err);
+    console.log("DONE", done);
+  });
+
+  var command = service.getByIdCommand(1);
+  command.execute((err, result) => {
     console.log("GET BY ID: ERROR: ", err);
     console.log("GET BY ID: result: ", result);
   });
 
-  var customer = { name: "Aaron Hanus" };
-  proxy.insert(customer, (err, result) => {
-    console.log("INSERT: ERROR: ", err);
+  command = service.insertCommand({ name: "Aaron Hanusa" });
+  command.execute((err, result) => {
+    console.log("INSERT:", err);
     console.log("INSERT: result: ", result);
-    proxy.getAll((err, result) => {
-      console.log("GET ALL: ERROR: ", err);
-      console.log("GET ALL: result: ", result);
-      result[1].name = "Aaron Hanusa";
-      proxy.update(result[1], (err, r) => {
-        console.log("UPDATE: ERROR: ", err);
-        console.log("UPDATE: result: ", r);
-        proxy.destroy(result.id, (err, result) => {
-          console.log("DELETE: ERROR: ", err);
-          console.log("DELETE: result: ", result);
-          proxy.getAll((err, result) => {
-            console.log("GET ALL: ERROR: ", err);
-            console.log("GET ALL: result: ", result);
-          });
-        });
-      });
-    });
   });
+
+  //var customer = { name: "Aaron Hanus" };
+  //proxy.insert(customer, (err, result) => {
+    //console.log("INSERT: ERROR: ", err);
+    //console.log("INSERT: result: ", result);
+    //proxy.getAll((err, result) => {
+      //console.log("GET ALL: ERROR: ", err);
+      //console.log("GET ALL: result: ", result);
+      //result[1].name = "Aaron Hanusa";
+      //proxy.update(result[1], (err, r) => {
+        //console.log("UPDATE: ERROR: ", err);
+        //console.log("UPDATE: result: ", r);
+        //proxy.destroy(result.id, (err, result) => {
+          //console.log("DELETE: ERROR: ", err);
+          //console.log("DELETE: result: ", result);
+          //proxy.getAll((err, result) => {
+            //console.log("GET ALL: ERROR: ", err);
+            //console.log("GET ALL: result: ", result);
+          //});
+        //});
+      //});
+    //});
+  //});
 
 
 
