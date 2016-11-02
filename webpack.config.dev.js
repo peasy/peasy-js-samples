ad8571b67b7ddb49262c85de2206ccf3d807925c
ahanusa <aaron.hanusa@gmail.com>
2016-11-02 08:17:57 -0500

    rename webpack.config.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -8,13 +8,13 @@ export default {
   entry: [
     'eventsource-polyfill', // necessary for hot reloading with IE
     'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
-    path.resolve(__dirname, 'client/app')
+    path.resolve(__dirname, 'client/index')
   ],
   target: 'web',
   output: {
-    path: __dirname + '/public/js', // Note: Physical files are only output by the production build task `npm run build`.
+    path: '/', // Note: Physical files are only output by the production build task `npm run build`.
     publicPath: '/',
-    filename: 'app.js'
+    filename: 'bundle.js'
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'client')
@@ -25,7 +25,7 @@ export default {
   ],
   module: {
     loaders: [
-      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+      {test: /\.js$/, include: path.join(__dirname, 'client'), loaders: ['babel']},
       {test: /(\.css)$/, loaders: ['style', 'css']},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
       {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
@@ -34,3 +34,4 @@ export default {
     ]
   }
 };
+
