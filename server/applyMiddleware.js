b8ee2a9a1c0109027ee4aa4714b717d27f77949c
ahanusa <aaron.hanusa@gmail.com>
2016-11-02 08:14:55 -0500

    wire up webpack dev and hot middleware

--- server/applyMiddleware.js
+++ server/applyMiddleware.js
@@ -1,8 +1,16 @@
 var bodyParser = require('body-parser');
 var express = require('express');
+var webpack = require('webpack');
+var config = require('../webpack.config.dev');
+var compiler = webpack(config);
 
 var applyMiddleware = function(app) {
   // MIDDLEWARE
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  }));
+  app.use(require('webpack-hot-middleware')(compiler));
   app.use(function(req, res, next) {
     for (var key in req.query) {
       req.query[key.toLowerCase()] = req.query[key];
