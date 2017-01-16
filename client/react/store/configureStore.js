260f0e6d558c0fad6667d68d48f40b2fb45aea27
ahanusa <aaron.hanusa@gmail.com>
2017-01-16 15:39:17 -0600

    configure reduxImmutableStateInvariant middleware in configureStore.js

--- client/react/store/configureStore.js
+++ client/react/store/configureStore.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/index';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
 export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState);
+  return createStore(
+    rootReducer, 
+    initialState, 
+    applyMiddleware(reduxImmutableStateInvariant())
+  );
 }; 
