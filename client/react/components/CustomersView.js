2ff730a0f48e38a08d0c10553116c9d3bf6ffdde
ahanusa <aaron.hanusa@gmail.com>
2017-01-16 12:51:30 -0600

    add name field and save inputs and wire up handlers

--- client/react/components/CustomersView.js
+++ client/react/components/CustomersView.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 class CustomersView extends React.Component {
 
+  constructor(props, context) {
+    super(props, context);
+    this.onNameChange = this.onNameChange.bind(this);
+    this.onSave = this.onSave.bind(this);
+    this.state = {
+      customer: { name: "" }
+    };
+  }
+
+  onNameChange(event) {
+    const customer = this.state.customer;
+    customer.name = event.target.value;
+    this.setState({ customer: customer });
+  }
+
+  onSave() {
+    console.log(`Saving ${this.state.customer.name}`);
+  }
+
   render() {
     return (
-      <div>CUSTOMERS</div>
+      <div>
+        <h1>Customers</h1>
+        <h2>Add customer</h2>
+        <input 
+          type="text"
+          onChange={this.onNameChange}
+          value={this.state.customer.name}
+          />
+        <input 
+          type="submit"
+          onClick={this.onSave}
+          value="Save"
+          />
+      </div>
     );
   }
 }
