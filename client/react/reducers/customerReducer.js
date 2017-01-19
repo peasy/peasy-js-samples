5a412f8cf28d1d6ddc27d83b9072ad8cd41db355
ahanusa <aaron.hanusa@gmail.com>
2017-01-18 19:45:26 -0600

    add support for loading customers

--- client/react/reducers/customerReducer.js
+++ client/react/reducers/customerReducer.js
@@ -4,6 +4,8 @@ export default function customerReducer(state = [], action) {
   switch (action.type) {
     case constants.actions.CREATE_CUSTOMER:
       return [...state, Object.assign({}, action.customer)];
+    case constants.actions.LOAD_CUSTOMERS_SUCCESS:
+      return action.customers;
     default:
       return state;
   }
