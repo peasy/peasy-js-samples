207425cc15f02e5c44ede31a47108174238795ed
ahanusa <aaron.hanusa@gmail.com>
2017-01-18 19:42:10 -0600

    remove add functionality

--- client/react/components/CustomersView.js
+++ client/react/components/CustomersView.js
@@ -1,28 +1,7 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import * as customerActions from '../actions/customerActions';
 
 class CustomersView extends React.Component {
-
-  constructor(props, context) {
-    super(props, context);
-    this.onNameChange = this.onNameChange.bind(this);
-    this.onSave = this.onSave.bind(this);
-    this.customerRow = this.customerRow.bind(this);
-    this.state = {
-      customer: { name: "" }
-    };
-  }
-
-  onNameChange(event) {
-    const customer = this.state.customer;
-    customer.name = event.target.value;
-    this.setState({ customer: customer });
-  }
-
-  onSave() {
-    this.props.dispatch(customerActions.createCustomer(this.state.customer));
-  }
 
   customerRow(customer, index) {
     return <div key={index}>{customer.name}</div>;
@@ -33,17 +12,6 @@ class CustomersView extends React.Component {
       <div>
         <h1>Customers</h1>
         {this.props.customers.map(this.customerRow)}
-        <h2>Add customer</h2>
-        <input 
-          type="text"
-          onChange={this.onNameChange}
-          value={this.state.customer.name}
-          />
-        <input 
-          type="submit"
-          onClick={this.onSave}
-          value="Save"
-          />
       </div>
     );
   }
