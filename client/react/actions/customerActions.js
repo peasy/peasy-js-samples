94a2b5ba3c2c08bff6151cc21149d7234eeb8b7c
ahanusa <aaron.hanusa@gmail.com>
2017-01-18 19:41:29 -0600

    add loadCustomers function

--- client/react/actions/customerActions.js
+++ client/react/actions/customerActions.js
@@ -1,5 +1,20 @@
 import constants from '../constants';
+import ordersDotCom from '../../businessLogic';
 
-export function createCustomer(customer) {
-  return { type: constants.actions.CREATE_CUSTOMER, customer: customer }
+function loadCustomersSuccess(customers) {
+  return { type: constants.actions.LOAD_CUSTOMERS_SUCCESS, customers: customers }
+};
+
+export function loadCustomers() {
+  return function(dispatch) {
+    var proxy = new ordersDotCom.dataProxies.CustomerDataProxy();
+    var service = new ordersDotCom.services.CustomerService(proxy);
+    var command = service.getAllCommand();
+    command.execute((err, result) => {
+      if (!err) {
+        return dispatch(loadCustomersSuccess(result.value));
+      }
+      return dispatch(loadCustomersFailure(err));
+    });
+  }
 };
