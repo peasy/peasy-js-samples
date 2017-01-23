af97877272de980711d209265c919aa207ff5f73
ahanusa <aaron.hanusa@gmail.com>
2017-01-23 15:51:37 -0600

    add SelectInput.js

---
+++ client/react/components/common/SelectInput.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const SelectInput = ({name, label, onChange, defaultOption, value, error, options}) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <div className="field">
+        <select
+          name={name}
+          value={value}
+          onChange={onChange}
+          className="form-control">
+          <option value="">{defaultOption}</option>
+          {
+            options.map((option) => {
+              return <option key={option.value} value={option.value}>{option.text}</option>
+            })
+          }
+        </select>
+        {error && <div className="alert alert-danger">{error}</div>}
+      </div>
+    </div>
+  );
+};
+
+export default SelectInput;
