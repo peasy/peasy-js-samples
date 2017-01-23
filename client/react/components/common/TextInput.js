47620087801763a62a0d93ea8bccc1eecefab56c
ahanusa <aaron.hanusa@gmail.com>
2017-01-23 15:51:08 -0600

    add TextInput.js

---
+++ client/react/components/common/TextInput.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const TextInput = ({name, label, onChange, placeholder, value, error}) => {
+  let wrapperClass = 'form-group';
+  if (error && error.length > 0) {
+    wrapperClass += ' has-error';
+  }
+
+  return (
+    <div className={wrapperClass}>
+      <label htmlFor={name}>{label}</label>
+      <div className="field">
+        <input
+          type="text"
+          name={name}
+          className="form-control"
+          placeholder={placeholder}
+          onChange={onChange} />
+        {error && <div className="alert alert-danger">{error}</div>}
+      </div>
+    </div>
+  );
+};
+
+export default TextInput;
