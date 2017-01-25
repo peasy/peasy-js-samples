8472b7a8a7e4748df3eb450fde959ae239dc9544
ahanusa <aaron.hanusa@gmail.com>
2017-01-25 06:55:01 -0600

    add logic to handle NaN

--- business_logic/rules/fieldTypeRule.js
+++ business_logic/rules/fieldTypeRule.js
@@ -4,7 +4,8 @@ var FieldTypeRule = Rule.extend({
   params: ['field', 'value', 'type'],
   functions: {
     _onValidate: function(done) {
-      if (this.value && typeof this.value !== this.type) {
+      if (isNaN(this.value)) { this.value = null; }
+      if (typeof this.value !== this.type) {
         this.association = this.field;
         this._invalidate(`Invalid type supplied for ${this.field}, expected ${this.type}`);
       }
