c393accbe0beb6041b91e32082b498f3019bea5c
ahanusa <aaron@buildbunker.com>
2018-07-22 10:57:08 -0500

    fuse customer list with details

--- client/angular/src/app/customer/customer-detail/customer-detail.component.ts
+++ client/angular/src/app/customer/customer-detail/customer-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Customer } from '../../customer';
 
 @Component({
   selector: 'app-customer-detail',
@@ -8,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class CustomerDetailComponent implements OnInit {
 
   constructor() { }
+
+  @Input()
+  selectedCustomer: Customer;
 
   ngOnInit() {
   }
