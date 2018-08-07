ab8ed2dcdd31a785ea32504653dcfc7b6aacdaa1
ahanusa <aaron@buildbunker.com>
2018-08-07 16:10:27 -0500

    stylings mostly complete

--- client/angular/src/app/app.component.ts
+++ client/angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'foo';
+
+  constructor(private router: Router) {}
+
+  isActive(path) {
+    console.log('PATH', path);
+    const x = this.router.url.indexOf(path) > -1;
+    console.log('INDEX OF', x);
+
+    return x;
+    // return this.router.url.indexOf(path) > -1;
+  }
 }
--- client/angular/src/app/product/product-list/product-list-viewmodel.ts
+++ client/angular/src/app/product/product-list/product-list-viewmodel.ts
@@ -1,7 +1,7 @@
 import { ListViewModelBase } from '../../list-view-model-base';
 import { Product } from '../../contracts';
 import { ProductService } from '../../services/product.service';
-import { Injectable } from '../../../../node_modules/@angular/core';
+import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class ProductListViewModel extends ListViewModelBase<Product> {
